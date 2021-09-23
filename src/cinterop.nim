32df83c3ca181b64510208d26a11d1869e555721
Ryan Blonna <n0.b741n37@gmail.com>
2021-09-23 17:39:48 +1000

    Rename lib folder to cinterop as suggested in nimble readme

--- src/cinterop.nim
+++ src/cinterop.nim
@@ -1,7 +1,7 @@
-import lib/decls
-import lib/exprs
-import lib/fieldaccessor
-import lib/pragmas
-import lib/types
-import lib/utils
+import cinterop/decls
+import cinterop/exprs
+import cinterop/fieldaccessor
+import cinterop/pragmas
+import cinterop/types
+import cinterop/utils
 export decls, exprs, fieldaccessor, pragmas, types, utils
